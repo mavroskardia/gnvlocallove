e2021c9df56989a9dcd5aa2c5f8444345c8e1cb8
Andrew Martin <andrew.martin@infotechinc.com>
2020-03-24 21:49:21 -0400

    Converted add business page's JS to use Places SDK

--- public/addbusiness.js
+++ public/addbusiness.js
@@ -7,29 +7,31 @@ async function lookup(e) {
   // wait until they've typed enough to be useful
   if (text.length < 3) return;
 
-  let paramsList = [
-    `key=${placesConfig.apiKey}`,
-    `input=${text}`,
-    'inputtype=textquery',
-    'fields=formatted_address,icon,name,photos,place_id'
-  ];
-  let params = paramsList.join('&');
-  let url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?${params}`;
+  let elt = document.getElementById('map');
+  let gnv = new google.maps.LatLng(29.6515, -82.3248);
+  let map = new google.maps.Map(elt, {center:gnv, zoom:15});
+  let service = new google.maps.places.PlacesService(map);
+  let request = {
+    query: text,
+    fields: ['formatted_address','icon','name','photos','place_id']
+  };
 
-  let response = await fetch(url);
-  let data = await response.json();
+  service.findPlaceFromQuery(request, (result, status) => {
 
-  let elt = document.getElementById('results');
-  elt.innerHTML = data.candidates.map(c => `
-    <li data-bag="${encodeURI(JSON.stringify(c))}">
-        <img src="${c.icon}">
-        <strong>${c.name}</strong>
-        <small>${c.formatted_address}</small>
-    </li>`).join();
+    let elt = document.getElementById('results');
+    elt.innerHTML = result.map(c => `
+      <li data-bag="${encodeURI(JSON.stringify(c))}">
+          <img src="${c.icon}">
+          <strong>${c.name}</strong>
+          <small>${c.formatted_address}</small>
+      </li>`).join();
 
-  document.querySelectorAll('#results li').forEach(li => {
-    li.addEventListener('click', choose);
-  })
+    document.querySelectorAll('#results li').forEach(li => {
+      li.addEventListener('click', choose);
+    });
+
+  });
+
 }
 
 function postMessage(text) {
