fec3825af5091f695f14c531fb3d2ecb32573733
Kyle <kyle.redon@gmail.com>
2020-03-26 23:07:48 -0400

    Misc css cleanup.

--- public/biz-card.js
+++ public/biz-card.js
@@ -9,17 +9,48 @@ class BizCard extends LitElement {
       }
 
       div.photo {
-        width: 380px;
+        width: 100%;
         height: 160px;
         background-repeat: no-repeat;
         background-size: cover;
       }
 
-      h3 {
-        font-weight: normal;
+      div.content {
         padding: 16px;
         text-align: center;
       }
+
+      div.content h2 {
+        font-weight: normal;
+        padding: 0;
+        margin: 16px 0;
+        text-align: center;
+      }
+
+      div.content .btn {
+        display: inline-block;
+        outline: none;
+        cursor: pointer;
+        color: #2ecc71;
+        padding: 16px;
+        margin-bottom: 16px;
+        background: transparent;
+        border: 1px solid #2ecc71;
+        border-radius: 32px;
+        text-decoration: none;
+      }
+      
+      div.content .btn:hover {
+        color: #fff;
+        background: #2ecc71;
+      }
+
+      div.content p {
+        padding: 0;
+        margin: 0;
+      }
+
+
     `;
   }
 
@@ -27,7 +58,12 @@ class BizCard extends LitElement {
     return html`
       <div>
         <div class="photo" style="background-image: url(${this.photo});"></div>
-        <h3>${this.name}</h3>
+        <div class="content">
+          <h2>${this.name}</h2>
+          <a href="#" class="btn">Purchase gift cards</a>
+          <p>Fames adipiscing faucibus dictumst condimentum ipsum mi curae. Bibendum lorem mi lectus nostra porta penatibus metus sollicitudin euismod. Aliquet convallis elit libero posuere dictum parturient nisl magna. Massa purus netus in sed si gravida malesuada. Natoque penatibus felis vel duis aliquet.</p>
+          
+          </div>
       </div>
     `;
   }
@@ -38,7 +74,13 @@ class BizCard extends LitElement {
       address: { type: String },
       gclink: { type: String },
       photo: { type: String },
-      icon: { type: String }
+      icon: { type: String },
+      ubereatslink: { type: String },
+      doordashlink: { type: String },
+      bitesquadlink: { type: String },
+      three52deliverylink: { type: String },
+      cflink: { type: String },
+      blurb: { type: String }
     }
   }
 }
