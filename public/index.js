17e432f625631049f83ada554d8c8dde5e2d2bee
Andrew Martin <andrew.martin@infotechinc.com>
2020-03-29 16:43:49 -0400

    Updated copy to represent all businesses, not just restaurants. Updated social media image version

--- public/index.js
+++ public/index.js
@@ -1,8 +1,3 @@
-// firebase.auth().onAuthStateChanged(user => { });
-// firebase.database().ref('/path/to/ref').on('value', snapshot => { });
-// firebase.messaging().requestPermission().then(() => { });
-// firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
-
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 import {firebaseConfig, getdb} from './globals';
