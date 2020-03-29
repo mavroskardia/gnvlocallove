ea0507128cdfe53b2a6f9d255443e6bcdcabf0d7
Andrew Martin <andrew.martin@infotechinc.com>
2020-03-29 11:21:53 -0400

    Switched to Google's Places autocomplete control, refactored a bunch, moved the Add Business link back to the top

--- public/addbusiness.js
+++ public/addbusiness.js
@@ -3,37 +3,7 @@ import 'firebase/firestore';
 import {getdb, firebaseConfig} from './globals';
 import './counting-text.js';
 
-async function lookup(e) {
-
-  // clear any existing message
-  postMessage('');
-
-  let text = e.target.value;
-  // wait until they've typed enough to be useful
-  if (text.length < 3) return;
-
-  let service = new google.maps.places.AutocompleteService();
-  let request = {
-    input: text,
-    location: new google.maps.LatLng(29.6515, -82.3248),
-    radius: 10,
-  };
-
-  service.getPlacePredictions(request, (result, status) => {
-    let elt = document.getElementById('results');
-    elt.innerHTML = result.filter(c=>c.types.indexOf('food') != -1)
-    .map(c => `
-      <li data-bag="${encodeURI(JSON.stringify(c))}">
-        ${c.description}
-      </li>`).join('');
-
-    document.querySelectorAll('#results li').forEach(li => {
-      li.addEventListener('click', choose);
-    });
-
-  });
-
-}
+var placeData;
 
 function postMessage(content) {
   document.getElementById('message').innerHTML = content;
@@ -41,37 +11,36 @@ function postMessage(content) {
 
 async function choose(e) {
 
-  let elt = e.target;
-  while (elt.nodeName !== 'LI') elt = elt.parentElement;
-
   // warn them if this placeId already exists in our db
-  let predata = JSON.parse(decodeURI(elt.dataset.bag));
+  placeData = this.getPlace();
   let db = getdb();
-  let qs = await db.collection('businesses').where('place_id', '==', predata.place_id).get();
+  let qs = await db.collection('businesses').where('place_id', '==', placeData.place_id).get();
 
   if (qs.size > 0) {
     postMessage('<div class="alert alert--error">Warning: this business already listed!</div>');
   }
 
-  // store the chosen field's data on the parent for later reference
-  let parent = document.getElementById('results');
-  parent.dataset.bag = elt.dataset.bag;
-  // wipe the list
-  parent.innerHTML = '';
-
-  // Update the search box to reflect the selected business
-  let search = document.getElementById('search');
-  search.value = elt.innerHTML.trim();
-
 }
 
 async function addBusiness() {
 
-  let db = getdb();
-  let elt = document.querySelector('#results');
-  let predata = JSON.parse(decodeURI(elt.dataset.bag));
-  let data = await build_data(predata);
+  let data = {
+    name: placeData.name,
+    address: placeData.formatted_address,
+    place_id: placeData.place_id,
+    icon: placeData.icon,
+    website: placeData.website,
+    photo: placeData.photos[0].getUrl({maxWidth:600}),
+    gclink: getLink('gclink'),
+    ubereatslink: getLink('ubereatslink'),
+    doordashlink: getLink('doordashlink'),
+    bitesquadlink: getLink('bitesquadlink'),
+    three52deliverylink: getLink('three52deliverylink'),
+    cflink: getLink('cflink'),
+    blurb: document.getElementById('blurb').value || ''
+  };
 
+  let db = getdb();
   await db.collection('businesses').add(data);
 
   postMessage('<div class="alert alert--success alert--fixed">Business successfully added.<a href="index.html"> &#x2190; Go back</a></div>');
@@ -84,40 +53,6 @@ function clear() {
   document.querySelectorAll('input').forEach(i => i.value = '');
   document.querySelectorAll('counting-text').forEach(ct => ct.value = '');
   document.getElementById('search').focus();
-}
-
-async function build_data(places_data) {
-
-  return new Promise(resolve => {
-
-    let gnv = new google.maps.LatLng(29.6515, -82.3248);
-    let mapParams = { center: gnv, zoom: 15 };
-    let map = new google.maps.Map(document.getElementById('map'), mapParams);
-    let service = new google.maps.places.PlacesService(map);
-    let request = {
-      placeId: places_data.place_id,
-      fields: ['formatted_address', 'icon', 'name', 'photo', 'website']
-    };
-
-    service.getDetails(request, async (result, status) => {
-      resolve({
-        name: result.name,
-        address: result.formatted_address,
-        place_id: places_data.place_id,
-        icon: result.icon,
-        photo: result.photos[0].getUrl({maxWidth:600}),
-        gclink: getLink('gclink'),
-        ubereatslink: getLink('ubereatslink'),
-        doordashlink: getLink('doordashlink'),
-        bitesquadlink: getLink('bitesquadlink'),
-        three52deliverylink: getLink('three52deliverylink'),
-        cflink: getLink('cflink'),
-        blurb: document.getElementById('blurb').value || '',
-        website: result.website
-      });
-
-    });
-  });
 }
 
 function getLink(id) {
@@ -143,7 +78,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   firebase.initializeApp(firebaseConfig);
   document.getElementById('add_business_btn').addEventListener('click', addBusiness);
-  document.getElementById('search').addEventListener('input', lookup);
-  document.getElementById('search').focus();
+
+  let searchBox = document.getElementById('search');
+  let autocomplete = new google.maps.places.Autocomplete(searchBox, {
+    types: ['establishment'],
+    bounds: new google.maps.LatLngBounds(
+      new google.maps.LatLng(29.476256, -82.656630), // SW corner of Alachua County
+      new google.maps.LatLng(29.935487, -82.061308)  // NE corner
+    ),
+    fields: ['place_id', 'formatted_address', 'icon', 'name', 'photo', 'website'],
+    strictBounds: true
+  });
+  autocomplete.addListener('place_changed', choose);
+
+  searchBox.focus();
 
 });
